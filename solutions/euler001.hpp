63072ad3022ae1d33ed2b43dc9cd3cdd5391c4ef
Denis <denisss025@gmail.com>
2015-03-13 09:44:16 +0300

    [001] Better overflow checks
     Add solution function without arguments (default limit == 1000u).
    
     More precise overflow checks.

--- solutions/euler001.hpp
+++ solutions/euler001.hpp
@@ -14,27 +14,41 @@ template <typename I>
 using make_unsigned_t = typename std::make_unsigned<I>::type;
 
 template <typename U> inline static auto solution(U limit) -> U {
-        limit -= 1;
-        auto sumN = [limit](U sum, U n) {
-                // Check for overflow
-                // (false positive are possible)
-                if (std::sqrt(((std::numeric_limits<U>::max() - sum) / n) * 2) < 
-                                static_cast<double>(limit) + 1.0) {
-                        throw std::overflow_error{"n: " + std::to_string(n)};
-                }
-                
-                const U limDivN = limit / n;
-                return sum + n * limDivN * (1 + limDivN) / 2;
-        };
-        
-        return sumN(sumN(U{}, 3) - sumN(U{}, 15), 5);
+	limit -= 1;
+
+	// limDiv03 * (1 + limDiv03) < pow((1 + limDiv03), 2) < maxU
+	constexpr U maxU = std::numeric_limits<U>::max();
+	const U limDiv03 = limit / 3;
+	if (std::sqrt(maxU) < 1 + limDiv03) 
+		throw std::overflow_error{"sum03"};
+
+	// Since (5 * limDiv15) <= limDiv03 and limDiv15 <= limDiv03,
+	// there is no need to check for overflow
+	const U limDiv15 = limit / 15;
+	const U sum03 = (limDiv03 * (1 + limDiv03) -
+			5 *  limDiv15 * (1 + limDiv15)) / 2;
+
+	// sum03 * 3 < maxU
+	if (maxU / 3 < sum03) throw std::overflow_error{"sum03 * 3"};
+	const U sum3 = 3 * sum03;
+
+	// sum3 + 5 * limDiv05 * (1 + limDiv05) / 2 < < sum3 + 5 * pow((1 + limDiv05), 2) < maxU
+	const U limDiv05 = limit / 5;
+	if (std::sqrt(((maxU - sum3) / 5) * 2) < 1 + limDiv05)
+		throw std::overflow_error{"sum3 + sum5"};
+	return sum3 + 5 * limDiv05 * (1 + limDiv05) / 2;
 }
 
 }  // namespace impl
 
+
 template <typename I> inline static auto solution(I limit) ->
                 impl::make_unsigned_t<I> {
         return impl::solution(static_cast<impl::make_unsigned_t<I>>(limit));
+}
+
+inline static auto solution() -> unsigned {
+	return impl::solution(1000u);
 }
 
 }  // namespace euler001
