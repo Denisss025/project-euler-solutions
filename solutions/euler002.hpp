f07c0d35d8a316c05fe7e691cc53af800a609957
Denis <denisss025@gmail.com>
2015-03-13 14:19:53 +0300

    [002] Rename function
    Function `add_fib` renamed to `sum_ef`. (Calculates sum for even Fibonacci numbers).

--- solutions/euler002.hpp
+++ solutions/euler002.hpp
@@ -4,15 +4,15 @@ namespace euler002 {
 
 namespace impl {
 
-static constexpr inline auto add_fib(uint64_t sum, uint64_t curr, uint64_t prev) -> uint64_t {
+static constexpr inline auto sum_ef(uint64_t sum, uint64_t curr, uint64_t prev) -> uint64_t {
 	return curr >= 4000000ul ? sum :
-		add_fib(sum + ((curr & 0x01) == 0 ? curr : 0ul), prev + curr, curr);
+		sum_ef(sum + ((curr & 0x01) == 0 ? curr : 0ul), prev + curr, curr);
 }
 
 }  // namespace impl
 
 static constexpr inline auto solution() -> uint64_t {
-	return impl::add_fib({}, 1ul, {});
+	return impl::sum_ef({}, {1}, {});
 }
 
 }  // namespace euler002
