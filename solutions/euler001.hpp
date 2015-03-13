a7879318d80246be74059518b6bdb5734abbf653
Denis <denisss025@gmail.com>
2015-03-13 14:30:20 +0300

    [001] Add description comment
    Add description comment to the header of the first Euler problem.

--- solutions/euler001.hpp
+++ solutions/euler001.hpp
@@ -1,3 +1,21 @@
+/*
+ * File: solutions/euler001.hpp
+ *
+ *  Copyright (c) 2015, Denis Novikov
+ *
+ *  Created on: Mar 13, 2015
+ *      Author: Denis Novikov
+ *     Licence: GNU v.3.0
+ * Description: see https://projecteuler.net/problem=1 for details.
+ *
+ * Problem (#001):
+ * If we list all the natural numbers below 10 that are multiples
+ * of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ */
+
 #pragma once
 
 #include <cmath>
